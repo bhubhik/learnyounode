29e1b5a124584551ecbd9da4e66f57ab3e97bc68
Anubhab Karki <bhubhik@gmail.com>
2022-07-19 14:56:10 +1000

    Superhero

---
+++ heroes.js
@@ -0,0 +1,7 @@
+const sh = require('superheroes');
+const hero = sh.random();
+
+exports.name = {
+  hero,
+  age: '12',
+};
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const hero = require('./heroes');
 const fs = require('fs');
 const myLog = 'console.log("This is appended text")';
-fs.writeFileSync('text.js', 'console.log("working")');
-fs.appendFileSync('text.js', myLog);
+fs.writeFileSync('text.txt', 'console.log("working")');
+fs.appendFileSync('text.txt', myLog);
+fs.appendFileSync('text.txt', hero.name.age);
+fs.appendFileSync('text.txt', hero.name.hero);
